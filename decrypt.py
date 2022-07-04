c200862f9656e669884102a66d8d6c609cc64e92
le-anh <lhanh@DESKTOP-4O79GMB.localdomain>
2022-07-04 20:12:11 +0900

    2022-07-04

--- decrypt.py
+++ decrypt.py
@@ -10,5 +10,10 @@ xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
 # print(bin((B1 << 1) ^ 0x1B))
 t = B0 ^ B1 ^ B2 ^ B3
 b = B0 ^ (t ^ xtime(B0 ^ B1))
-print(bin(B0))
-print(hex(b))
+# print(bin(B0))
+# print(hex(b))
+
+c = list([1, 2, 3, 4])
+print(c)
+c.append(c.pop(0))
+print(c)
