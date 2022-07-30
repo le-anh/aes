3bb1f29ba8e35eb89b13b05a4e0a635b1fc4327f
le-anh <lhanh@DESKTOP-4O79GMB.localdomain>
2022-07-30 18:43:26 +0900

    2022-07-30

--- file_reader.py
+++
@@ -1,9 +0,0 @@
-# File reader class
-class FileReader:
-    # Read file
-    @staticmethod
-    def Read(file_in: str) -> bytes:
-        with open(file_in, "rb") as fin:
-            file_data = fin.read()
-
-        return file_data
--- file_writer.py
+++
@@ -1,8 +0,0 @@
-# File writer class
-class FileWriter:
-    # Write file
-    @staticmethod
-    def Write(file_out: str,
-              data: bytes) -> None:
-        with open(file_out, "wb") as fout:
-            fout.write(data)
