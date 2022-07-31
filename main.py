d3d1212cbcb195ae4ed0afd196a10cd28e9413eb
le-anh <lhanh@DESKTOP-4O79GMB.localdomain>
2022-07-31 20:58:11 +0900

    2022-07-31

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import csv
 from datetime import datetime
 from lib.file_encrypter import FileEncrypter
@@ -13,38 +14,33 @@ def export_to_csv(data_row = ''):
 
 def file_encrypt(file_name):
     file_encrypter = FileEncrypter()
-    t0 = datetime.now()
     file_encrypter.Encrypt("original/" + file_name + ".txt", password)
-    t1 = datetime.now()
     file_encrypter.SaveTo("result/enc_" + file_name)
-    enc_time = (t1-t0).total_seconds()*1000.0
+    enc_time = file_encrypter.GetEncryptedTime()
     print(f"Encrypt success (file {file_name}: {enc_time} ms)!")
     return enc_time
 
 def file_decrypt(file_name):
     file_decrypter = FileDecrypter()
-    t0 = datetime.now()
     file_decrypter.Decrypt("result/enc_" + file_name, password)
-    t1 = datetime.now()
     file_decrypter.SaveTo("result/dec_" + file_name +".txt")
-    dec_time = (t1-t0).total_seconds()*1000.0
+    dec_time = file_decrypter.GeDecryptedTime()
     print(f"Decrypt success (file {file_name}: {dec_time} ms)!")
     return dec_time
 
 def file_enc_dec():
-    file_encrypt("text")
-    file_decrypt("text")
-    # for i in range(10):
-    #     print(f"Iterating: {i+1}")
-    #     data_row = [i+1]
-    #     for fn in range(8):
-    #         file_name = str(fn+1)
-    #         data_row.append(file_encrypt(file_name))
-    #         data_row.append(file_decrypt(file_name))
-    #     export_to_csv(data_row)
+    for i in range(10):
+        print(f"Iterating: {i+1}")
+        data_row = [i+1]
+        for fn in range(8):
+            file_name = str(fn+1)
+            data_row.append(file_encrypt(file_name))
+            data_row.append(file_decrypt(file_name))
+        export_to_csv(data_row)
 
 def run():
     file_enc_dec()
+
 
 if __name__ == "__main__":
     run()
