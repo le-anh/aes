1a01ac6ab0ad9ccda518565b51881391662eaef1
le-anh <lhanh@DESKTOP-4O79GMB.localdomain>
2022-07-31 19:32:58 +0900

    2022-07-31

--- lib/file_decrypter.py
+++ lib/file_decrypter.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from typing import List, Optional, Union
+from numpy import longlong
 from .key_generator import KeyGenerator
 from .file_reader import FileReader
 from .file_writer import FileWriter
@@ -9,23 +11,31 @@ class FileDecrypter:
     data_encrypted: bytes
     iv: bytes
     iv_size: int
+    dec_time: longlong
 
     # Contructor
     def __init__(self) -> None:
         self.data_encrypted = b""
         self.iv = b""
         self.iv_size = AES.block_size
+        self.dec_time = 0
     
     # Decrypt
     def Decrypt(self, file_in: str, passwords: List[Union[str, bytes]], salt: Optional[Union[str, bytes]] = None, itr_num: Optional[int] = None)->None:
-        key_generator = KeyGenerator(passwords, salt, itr_num)
-        # Read file
-        file_data = FileReader.Read(file_in)
-        self.iv = file_data[-self.iv_size:]
-        file_data = file_data[:len(file_data)-self.iv_size]
-        # Decrypt it
-        decrypter = AES.new(key_generator.GetMasterKey(), AES.MODE_CBC , self.iv)
-        self.data_encrypted = unpad(decrypter.decrypt(file_data), AES.block_size)
+        try:
+            key_generator = KeyGenerator(passwords, salt, itr_num)
+            # Read file
+            file_data = FileReader.Read(file_in)
+            self.iv = file_data[-self.iv_size:]
+            file_data = file_data[:len(file_data)-self.iv_size]
+            # Decrypt it
+            decrypter = AES.new(key_generator.GetMasterKey(), AES.MODE_CBC, self.iv)
+            t0 = datetime.now()
+            self.data_encrypted = unpad(decrypter.decrypt(file_data), AES.block_size)
+            t1 = datetime.now()
+            self.dec_time = (t1-t0).total_seconds()*1000.0
+        except(ValueError, KeyError):
+            print("Incorrect decryption")
     
     # Get decrypted data
     def GetDecryptedData(self)->bytes:
@@ -34,4 +44,8 @@ class FileDecrypter:
     # Save to file
     def SaveTo(self, file_out: str)->None:
         FileWriter.Write(file_out, self.data_encrypted)
+
+    # Get encryption time
+    def GeDecryptedTime(self):
+        return self.dec_time
     
--- lib/file_encrypter.py
+++ lib/file_encrypter.py
@@ -1,17 +1,22 @@
+from datetime import datetime
 from typing import List, Optional, Union
+from numpy import longlong
 from .key_generator import KeyGenerator
 from .file_reader import FileReader
 from .file_writer import FileWriter
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
 
 class FileEncrypter:
     date_encrypted: bytes
     iv: bytes
+    enc_time: longlong
 
     def __init__(self) -> None:
         self.date_encrypted = b""
         self.iv = b""
+        self.enc_time = 0
     
     def Encrypt(self, file_in: str, passwords: List[Union[str, bytes]], salt: Optional[Union[str, bytes]] = None, itr_num: Optional[int] = None)->None:
         # Generate key
@@ -20,8 +25,11 @@ class FileEncrypter:
         file_data = FileReader.Read(file_in)
         # Encrypt it
         encrypter = AES.new(key_generator.GetMasterKey(), AES.MODE_CBC)
+        t0 = datetime.now()
         self.date_encrypted = encrypter.encrypt(pad(file_data, AES.block_size))
+        t1 = datetime.now()
         self.iv = encrypter.iv
+        self.enc_time = (t1-t0).total_seconds()*1000.0
     
     def GetEncryptedData(self)->bytes:
         return self.date_encrypted
@@ -31,3 +39,6 @@ class FileEncrypter:
     
     def SaveTo(self, file_out: str)->None:
         FileWriter.Write(file_out, b''.join([self.date_encrypted, self.iv]))
+
+    def GetEncryptedTime(self):
+        return self.enc_time
